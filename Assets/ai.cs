38e93d281f769f48bb2ac72f16f9ea2ef8679a81
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2023-05-23 17:14:33 +0330

    A capsule and Plane added, still no NavmeshAi and

---
+++ Assets/ai.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class ai : MonoBehaviour
+{
+    public ai hello;
+    //public NavMesh
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  hello.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
