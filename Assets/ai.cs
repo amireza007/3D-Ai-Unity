496bfdb3f7bd8165f602ea009f2c7cd0a887c22e
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2023-05-24 11:26:48 +0330

    Added models from unity assets

--- Assets/ai.cs
+++ Assets/ai.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 public class ai : MonoBehaviour
 {
-    public ai hello;
+  
     //public NavMesh
     // Start is called before the first frame update
     void Start()
